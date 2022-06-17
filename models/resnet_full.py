4966d669d2beb3e9b005de7b3319a54b68e4a79f
NanoCAD Projects <nanoproj@nanocad-server-testbed.ee.ucla.edu>
2022-06-17 06:01:02 -0700

    Changes to full precision resnet for residual computation.

--- models/resnet_full.py
+++ models/resnet_full.py
@@ -78,30 +78,31 @@ class BasicBlock(nn.Module):
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = norm_layer(planes)
-        self.act = nn.ReLU(inplace=True)
+        self.act = nn.ReLU()
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
         self.stride = stride
+        self.bn3 = norm_layer(planes)
 
     def forward(self, x: Tensor) -> Tensor:
         #identity = x
         identity = x.clone()
         #identity.retain_grad()
         
-        out = self.act(x)
-        out = self.conv1(out)
+        out = self.conv1(x)
         out = self.bn1(out)
-
         out = self.act(out)
+
         out = self.conv2(out)
         out = self.bn2(out)
 
         if self.downsample is not None:
             identity = self.downsample(x)
+            identity = self.bn3(identity)
 
         out += identity
-        #out = self.act(out)
+        out = self.act(out)
 
         return out
 
